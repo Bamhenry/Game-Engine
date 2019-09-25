25e674e175704c27d7f57c2d9bbe93813552c664
Bamhenry <32941634+Bamhenry@users.noreply.github.com>
2019-09-25 08:38:24 -0500

    Add project files.

---
+++ Vector3D.h
@@ -0,0 +1,19 @@
+#pragma once
+class Vector3D
+{
+public:
+	float x;
+	float y;
+	float z;
+
+	Vector3D();
+	~Vector3D();
+
+	Vector3D(float xCoord, float yCoord, float zCoord);
+	Vector3D(const Vector3D& v);
+	Vector3D& operator= (const Vector3D& rhs);
+
+private:
+
+};
+
